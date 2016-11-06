e3ec973df8409b3396da91a86fb37157f1048809
zengyanzi <zengyanzi@users.noreply.github.com>
2016-11-07 10:42:47 +1300

    front end page copy from firstp2p.com

---
+++ wx/index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<title>first P2P </title>
+<link rel="apple-touch-icon-prcomposed" href="http://assets.firstp2p.com/skins/default/images/favicon.png" type="image/x-icon" >
+<link rel="shorcut"  href="http://assets.firstp2p.com/skins/default/images/favicon.png" type="image/x-icon">
+<meta name="keywords" content="P1P"/>
+<meat name="description" content="safe financy solution" />
+
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
+<script src="js/jquery.js"></script>
+<link rel="stylesheet" href="css/swiper.min.css">
+<script src="js/swiper.min.js"></script>
+
+<!-- 3，加载bootstrap核心Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="css/index.css"/>
+
+
+<script >
+	var status_switch=0;
+	var VAR_FILESIZE=1.5;
+</script>
+<link title="firstP2P" type="application/rss+xml" rel="alternate" href="http://www.firstp2p.com/rss">
+<script type="text/javascript">
+	var APP_ROOT='';
+	var send_span=60000;
+	var USER_INFO=0;
+</script>
+</head>
+<body>
+	<header >
+		<div class="top">
+			<ul>
+				<li class="border_1"><a href="#">Users Manual</a></li>
+				<li ><a class="color_green" href="#">Register</a></li>
+				<li>Hello, Please <a href="#">Login</a></li>
+			</ul>
+		</div>
+		<div class="navicate">
+			<div class="w1100">
+				<a  class="logo" href="#"></a>
+				<div class="my_account">
+					<a href="#"><i class="glyphicon glyphicon-user"></i>My account<i class="glyphicon glyphicon-triangle-bottom"></i></a>
+				</div>
+				<ul class="navi">
+					<li><a class="selected" href="#">Home</a></li>
+					<li><a href="#">Financial</a></li>
+					<li><a href="#">VIP</a></li>
+					<li><a href="#">Funding</a></li>
+					<li><a href="#">Mobile</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+	<div class="index">
+		<section>
+			<div class="swiper-container">
+		    	<div class="swiper-wrapper">
+			        <div class="swiper-slide"><div class="slide1pic"></div></div>
+			        <div class="swiper-slide"><div class="slide2pic"></div></div>
+			        <div class="swiper-slide"><div class="slide3pic"></div></div>
+   				</div>
+   				<!-- 如果需要分页器 -->
+				    <div class="swiper-pagination"></div>
+				    
+
+   			</div>
+		</section>
+		<section class="benefit">
+			<p class="announcement w1100 f18">
+				<span>the benifit in pocket: </span>
+				<span class="color_red f18"> $1,234,567</span> 
+				<span>the benefit on the way is: </span> 
+				<span class="color_red f18"> $765,432</span>
+			</p>
+		</section>
+		<section class="main">
+			<div class="w1100">
+				<div class="product_type clearfix">
+					<span class="type_name">FirstP2P</span>
+					<ul class="type_navi">
+						<li><a class="selected" href="#">ALL(123455)</a></li>
+						<li><a href="#">ALL(123455)</a></li>
+						<li><a href="#">ALL(123455)</a></li>
+					</ul>
+				</div>
+				<div class="product bg_white mb30">
+					<div class="pro1">
+						<h3 class="f16">
+							<div class="prod1icon"></div>
+							<a href="#">At least 100RMB652-11</a></h3>
+						<div class="prod_detail w360">
+							<p><span>Annual rate:</span><span class="color_red f18">10.00%</span></p>
+							<P><span>Total: </span><span> 1 Million</span></P>
+						</div>
+						<div class="prod_detail w265">
+							<p><span>Invest:</span><span class="color_red f18"> 3 Months</span></p>
+							<P><span>Benefit: </span><span> Once</span></P>
+						</div>
+						<div class="prod_detail w265">
+							<div class="invest"><span class="investcontent">Invest process</span>
+								<div class="bigprogress">
+									<div class="smallprogress"></div>
+								</div>
+							 	<span class="color_gray f16">8.00%</span>
+							</div>
+							<p><span>Rest: </span><span> 533,333</span></p>
+						</div>
+						<div>
+							<a href="#" class="btn btn-danger investbtn">Invest</a>
+						</div>
+					</div>
+					
+				</div>
+				<div class="w1100 clearfix">
+					<div class="news mb30 clearfix">
+						<div class="newslist bg_white">
+							<div class="newtitle">
+								<h2 >
+								Media news
+								</h2>
+								<a class="more" href="#"><i class="glyphicon glyphicon-chevron-right"></i></a>
+							</div>
+							<div class="con">
+								<div class="new_img">
+									<a href="#"><img src="image/mtbd.jpg" height="215" width="324"></a>
+									<p><a href="#">some information about P2P</a></p>
+								</div>
+								<ul class="art_list">
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span>
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+								</ul>
+							</div>
+				
+
+						</div>
+					</div>
+
+					<div class="news mb30 ">
+						<div class="newslist bg_white">
+							<div class="newtitle">
+								<h2 >
+								Media news
+								</h2>
+								<a class="more" href="#"><i class="glyphicon glyphicon-chevron-right"></i></a>
+							</div>
+							<div class="con">
+								<div class="new_img">
+									<a href="#"><img src="image/mtbd.jpg" height="215" width="324"></a>
+									<p><a href="#">some information about P2P</a></p>
+								</div>
+								<ul class="art_list">
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span>
+									<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+						<div class="news mb30 ">
+							<div class="newslist bg_white">
+								<div class="newtitle">
+									<h2 >
+									Return Notice
+									</h2>
+									<a class="more" href="#"><i class="glyphicon glyphicon-chevron-right"></i></a>
+								</div>
+								<div class="con">
+									<ul class="notice_list">
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span>
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span>
+										<li class="art_title"><a href="#">adf sldfsdf ljkllsd </a><span class="date">2016-01-27</span></li>
+									</ul>
+								</div>
+							</div>
+					</div>
+				</div>
+				<div class="partner clearfix">
+					<div class="title">
+						<h2>
+							Partners
+						</h2>
+					</div>
+						<div class="swiper-container">
+					        <div class="swiper-wrapper">
+					            <div class="swiper-slide">
+					            	<ul class="partner1">
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+
+					            	</ul>
+
+					            </div>
+					            <div class="swiper-slide">
+					            	<ul class="partner1">
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		<li><a href="#"><span></span></a></li>
+					            		
+					            	</ul>
+					            </div>
+					            <div class="swiper-slide">Slide 3</div>
+					        </div>
+					        <!-- Add Arrows -->
+					        <div class="swiper-button-next" style="background:url('image/icon_bg.png');width:12px;height:23px;background-position:-281px 0;"></div>
+					        <div class="swiper-button-prev" style="background:url('image/icon_bg.png');width:12px;height:23px;background-position:-251px 0;"></div>
+	    				</div>
+				</div>
+
+			</div>
+		</section>
+		<section class="step">
+			<div class="step_img">
+				<img src="image/step.png" height="121" >
+			</div>
+			<a class="registernow"href="#">Register now</a>
+		</section>
+		<section class="footer">
+			<div class="w1100 clearfix">
+				<ul class="footerleft">
+					<li class="clearfix">
+						<a class="logo" href="#"></a>
+					</li>
+					<li class="contact">
+						Hotline<span>(work time 7:00-23:00)</span>
+					</li>
+					<li class="f24">400-890-9888</li>
+					</div>
+				</ul>
+
+			</div>
+		</section>
+		<div class="layapptop">
+			<div class="app">
+				<a class="app_btn"href="javascript:void(0)"></a>
+				<div class="app_box">
+					<i class="icon_a"></i>
+					<div class="app_tab_con">
+						<img src="image/download.png" >
+					</div>
+					<h2>Please download</h2>
+				</div>
+			</div>
+			<div class="server_box">
+				<a class="server" href="javascript:void(0)"></a>
+				<div class="server_con">
+					<i class="icon_a"></i>
+					<h2>400-890-9888</h2>
+					<p>worktime 7:00-23:00</p>
+				</div>
+			</div>
+			<a class="backtop" href="#"></a>
+		</div>
+	</div>
+	<script>        
+  var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+    	prevButton: '.swiper-button-prev',
+    	autoplay: 3500,
+    });
+  </script>
+</body>
+</html>
